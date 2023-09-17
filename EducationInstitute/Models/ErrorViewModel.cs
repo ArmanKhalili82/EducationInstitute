2165aca7810688f271e237034e1b1070581611ca
ArmanKhalili82 <hykhalili@yahoo.com>
2023-09-17 19:49:05 +0200

    Add project files.

---
+++ EducationInstitute/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EducationInstitute.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ EducationInstitute/Models/Login.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EducationInstitute.Models
+{
+    public class Login
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Username is requried!")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Password is requried!")]
+        public string Password { get; set; }
+    }
+}
