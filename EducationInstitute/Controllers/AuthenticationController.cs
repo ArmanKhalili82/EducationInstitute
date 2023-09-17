c2090fa34e59e5dc761a7fe16b869f14a9120bf4
alikh <alikh@outlook.com>
2023-09-17 22:32:06 +0200

    commented

--- EducationInstitute/Controllers/AuthenticationController.cs
+++ EducationInstitute/Controllers/AuthenticationController.cs
@@ -1,5 +1,6 @@
 using EducationInstitute.DTO;
 using EducationInstitute.Models;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,9 +9,17 @@ namespace EducationInstitute.Controllers
     [Route("[controller]")]
     public class AuthenticationController : ControllerBase
     {
+        //private readonly IUserService _userService;
+        //public AuthenticationController(IUserService userService)
+        //{
+        //    _userService = userService;
+        //}
+
         [HttpPost("Login")]
         public async Task<ActionResult> Login([FromBody] LoginDto login)
         {
+            //var user = await _userService.Login(login);
+
             var db = new ApplicationDbContext();
 
             var user = await db.Users.Where(u => u.Username == login.UserName && u.Password == login.Password)
